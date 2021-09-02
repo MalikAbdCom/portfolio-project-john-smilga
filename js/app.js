a3673defc55b8be9f2ff275e220150c358fb9ae8
malikabdcom <emailmalikabdcom@gmail.com>
2021-09-02 14:41:25 +0700

    menambahkan html di bagian header --index.html

--- js/app.js
+++ js/app.js
@@ -5,18 +5,18 @@ const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 80) {
-        navbar.classList.add("navbar-fixed");
-    } else {
-        navbar.classList.remove("navbar-fixed");
-    }
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
 });
 // show sidebar
 tombolNav.addEventListener("click", function () {
-    sidebar.classList.add("show-sidebar");
+  sidebar.classList.add("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
-    sidebar.classList.remove("show-sidebar");
+  sidebar.classList.remove("show-sidebar");
 });
 // set year
 date.innerHTML = new Date().getFullYear();
